131e57c8c059fdadc7000d5cc65bf69a67b6bff0
Atharva Nitin Pawar <123879019+Atharva0126@users.noreply.github.com>
2026-06-10 22:50:39 +0530

    Update ai_helper.py

--- utils/ai_helper.py
+++ utils/ai_helper.py
@@ -1,1 +1,35 @@
+from openai import OpenAI
+import streamlit as st
+
+client = OpenAI(
+api_key=st.secrets["OPENAI_API_KEY"]
+)
+
+def ask_ai(question):
+
+```
+response = client.chat.completions.create(
+    model="gpt-4.1-mini",
+    messages=[
+        {
+            "role":"system",
+            "content":"""
+            You are a Mathematics Tutor.
+
+            Explain:
+            - Simple language
+            - Maximum 100 words
+            - Give one example
+            - Student friendly
+            """
+        },
+        {
+            "role":"user",
+            "content":question
+        }
+    ]
+)
+
+return response.choices[0].message.content
+```
 
